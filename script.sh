f0e7934d1dd5a1fca9ae7e7537daca9c4cd10dc1
omarbv <bvomar@gmail.com>
2014-03-18 23:33:02 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,6 +7,7 @@ do
 echo "[*] Mobile number: $i" >> data
 echo "[--- " >> data
 curl -d "method=sms&to=34$i&in=$i&cc=34&mcc=000&mnc=000" https://r.whatsapp.net/v1/code.php >> data
-let i++status="success-sent"
+let i++
+status="success-sent"
 echo "---]" >> data
 done
